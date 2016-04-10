1407ee25510c5b14f94541db1eac5d04f1db0f62
Sazanovich Alex <sazanovich.notify@gmail.com>
2016-04-10 12:41:19 +0300

    Загружена библиотека sedjevik  и запущен тестовый метод

---
+++ src/sazanovich/learn/java/Main.java
@@ -0,0 +1,17 @@
+package sazanovich.learn.java;
+
+import com.brianmcmichael.StdOut;
+import com.brianmcmichael.StdRandom;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        StdOut.println("Hello World");
+        Double number = StdRandom.uniform();
+        System.out.println("Число " + number * 10);
+
+    }
+
+
+}
